846e5a592acb9cda520940733a897df273f72c6b
Paul Hayes <water7earth@gmail.com>
2026-02-09 21:02:06 +1100

    Add initial project structure for MathStorm game, including HTML, JavaScript, and configuration files. Added .gitignore to exclude build artifacts and environment files.

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MathStorm from '../MathStorm'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <MathStorm />
+)
